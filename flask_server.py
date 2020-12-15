1c06b86a5bbb0e87bdbd54da728b81d1ab3b3162
Ed <edrabbit@edrabbit.com>
2020-12-15 00:54:26 +0000

    Add support for First Time Ask points

--- flask_server.py
+++ flask_server.py
@@ -14,8 +14,13 @@ AUTH = requests.auth.HTTPDigestAuth(settings.INDIGO_USER, settings.INDIGO_PASS)
 
 def _get_variable_value(variable):
     uri = settings.INDIGO_URI_VAR + variable + '.json'
+    print "about to get %s" % uri
     r = requests.get(uri, auth=AUTH)
-    return r.json()['value']
+    print r
+    try:
+        return r.json()['value']
+    except ValueError:
+        return "Not Found"
 
 
 def _set_variable_value(variable, value):
@@ -27,6 +32,25 @@ def _set_variable_value(variable, value):
     else:
         return False
 
+
+def fetch_first_time_ask_score():
+    fta_score = _get_variable_value('first_time_ask_score')
+    return fta_score
+
+def reset_first_time_ask_score():
+    print "in reset_first_time_ask_score"
+    now = datetime.datetime.now()
+    _set_variable_value('first_time_ask_last_reset', now.isoformat())
+    _set_variable_value('first_time_ask_score', '0')
+
+def fetch_last_fta_reset(dt=True):
+    # returns datetime object with datetime of last reset
+    first_time_ask_last_reset = _get_variable_value('first_time_ask_last_reset')
+    if dt:
+        last_reset_dt = iso8601.parse_date(first_time_ask_last_reset)
+        return last_reset_dt
+    else:
+        return first_time_ask_last_reset
 
 def fetch_cat_bathroom_score():
     cb_score = _get_variable_value('cat_bathroom_score')
@@ -50,7 +74,6 @@ def reset_cat_bathroom_score():
 @app.route("/")
 def root():
     return flask.render_template('index.html')
-
 
 def display_cat_bathroom(values={}):
     values['score'] = fetch_cat_bathroom_score()
@@ -79,5 +102,36 @@ def cat_bathroom(action=None):
         else:
             return "Unsupported action: %s" % action
 
+def display_fta(values={}):
+    print "In display_fta"
+    values['score'] = fetch_first_time_ask_score()
+    print "score fetched"
+    last_reset = fetch_last_fta_reset(dt=True)
+    print "last_reset fetched"
+    values['last_date'] = (
+        last_reset.isoformat().replace("T", " ").split('.')[0])
+
+    today = datetime.datetime.now()
+    values['days_since'] = (today.date() - last_reset.date()).days
+
+    return flask.render_template(
+        'fta.html', values=values, title="First Time Ask")
+
+
+@app.route('/fta')
+@app.route('/fta/')
+@app.route('/fta/<action>')
+def fta(action=None):
+    print "FTA"
+    if not action:
+        return display_fta()
+    else:
+        if action == "reset":
+            reset_first_time_ask_score()
+            return flask.redirect(flask.url_for('fta'))
+        else:
+            return "Unsupported action: %s" % action
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
